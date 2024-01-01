0e8ac535e529c34219fd51d26a179b6659956491
sahilnagpal <thisissahilnagpal@gmail.com>
2024-01-01 16:12:12 -0500

    happy new year 2024

---
+++ leetcode/easy-question/1929. Concatenation of Array.py
@@ -0,0 +1,11 @@
+def getConcatenation(self, nums):
+    """
+    :type nums: List[int]
+    :rtype: List[int]
+    """
+    self.nums = nums
+    return self.nums + self.nums
+
+
+name = "1929. Concatenation of Array"
+print(name.replace(" ","%"))
