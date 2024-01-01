9a4afa5b928848d7847f3c0c7bfacf1e523d2278
sahilnagpal <thisissahilnagpal@gmail.com>
2024-01-01 15:45:02 -0500

    happy new year 2024

--- leetcode/easy-question/2965. Find Missing and Repeated Values.py
+++ leetcode/easy-question/2965. Find Missing and Repeated Values.py
@@ -1,26 +1,20 @@
 from collections import Counter
 
-grid = [[1,3],[2,2]]
 
+def findMissingAndRepeatedValues(self, grid):
 
+    self.grid = sum(grid,[])
 
+    # missing number
 
-grid = sum(grid,[])
+    all_value_grid = [x for x in range(1,len(grid)+1)]
+    answer = list(set(all_value_grid)-set(grid))
 
-# missing number
+    # repeated value
+    for key,value in Counter(grid).items():
+        if value > 1:
+            answer.insert(0,key)
 
-all_value_grid = [x for x in range(1,len(grid)+1)]
-print(all_value_grid)
-
-answer = list(set(all_value_grid)-set(grid))
-
-
-# repeated value
-for key,value in Counter(grid).items():
-    if value > 1:
-        answer.insert(0,key)
-
-
-print(answer)
+    return answer
 
 
